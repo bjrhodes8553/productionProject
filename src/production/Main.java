5144f8721b03e0b329f49d98f10e2126d203d491
bjrhodes8553 <42774947+bjrhodes8553@users.noreply.github.com>
2019-09-28 18:45:08 -0400

    style checks(0)

--- src/production/Main.java
+++ src/production/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Production Task");
+    primaryStage.setTitle("Production Task: ");
     primaryStage.setScene(new Scene(root, 400, 300));
     primaryStage.show();
   }
